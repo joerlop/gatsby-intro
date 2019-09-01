562a40439914f3d782e6cfa787ecf295f2309610
joerlop <jj.erlich155@gmail.com>
2019-09-01 12:36:58 -0500

    Hero image displayed as a sharp image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const ImageBackground = styled('div')`
-  background-image: url(/images/nyc.jpg);
+const ImageBackground = styled(BackgroundImage)`
   background-position: top 20% center;
   background-size: cover;
   height: 50vh;
@@ -21,6 +21,7 @@ const TextBox = styled('div')`
   justify-content: flex-end;
   padding: 0 calc((100vw - 550px) / 2) 2rem;
   width: 100%;
+  margin-top: 0;
 
   h1 {
     text-shadow: 1px 1px 3px #eeddff66;
@@ -39,8 +40,20 @@ const TextBox = styled('div')`
 `;
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "nyc.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <ImageBackground>
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
       <TextBox>
         <h1>Frontend Masters + Gatsby &hearts;</h1>
         <p>
